281067084470e79efb68c5d9a1b2adc1a398f302
Daniel Lamb <daniellamb113@gmail.com>
2019-09-12 06:25:22 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,10 +1,7 @@
 def square_array(array)
-numbers = [1,2,3]
-square_array(numbers).each
-# => [1,4,9])
+.each
 
-new_numbers= [9,10,16,25].collect
 
-square_array(new_numbers).inject
-# => [81,100,256,625]
+
+
 end
