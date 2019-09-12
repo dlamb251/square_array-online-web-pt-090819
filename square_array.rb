f51f4a1820abed6b6f03037c3914714534dea02b
Daniel Lamb <daniellamb113@gmail.com>
2019-09-12 06:24:47 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -3,7 +3,7 @@ numbers = [1,2,3]
 square_array(numbers).each
 # => [1,4,9])
 
-new_numbers= [9,10,16,25].collection_select
+new_numbers= [9,10,16,25].collect
 
 square_array(new_numbers).inject
 # => [81,100,256,625]
